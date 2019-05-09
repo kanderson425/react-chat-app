563d32394c7c9bb997f00b3b978a2db208366899
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-09 15:33:03 -0500

    Updates the Chatkit dependency to Chatkit-client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import Chatkit from '@pusher/chatkit';
+import Chatkit from '@pusher/chatkit-client';
 import './App.css';
 import MessageList from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
 import RoomList from './components/RoomList';
 import NewRoomForm from './components/NewRoomForm';
+import {tokenUrl, instanceLocator} from './config';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'kyleanderson',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId:'31220080',
+        hooks: {
+          onNewMessage: message => {
+            console.log('message.text: ', message.text);
+          }
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <div className="app">
