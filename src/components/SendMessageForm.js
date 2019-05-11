5f5112dc6cc264bc84ef0e1f5ef54feee10bee3b
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-10 23:53:17 -0500

    Adds style.css properties for CSS grid

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component {
+
+    handleChange(e) {
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <form className='send-message-form'>
                 <input
-                    placeholder="SendMesageForm"
+                    onChange={this.handleChange}
+                    placeholder="Type your message and hit ENTER"
                     type="text" />
             </form>
         )
     }
 }
+
 export default SendMessageForm;
