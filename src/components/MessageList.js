be1219ad8dc7c9b2863e1ad3a5a3295466267cf2
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-13 21:48:20 -0500

    Adds the dummy data into the message list component

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,33 +2,41 @@ import React from 'react';
 import Message from './Message';
 import '../style.css';
 
-// const DUMMY_DATA = [
-//     {
-//         senderId: 'perborgen',
-//         text: 'Hey, how is it going?'
-//     },
-//     {
-//         senderId: 'janedoe',
-//         text: 'Great! How about you?'
-//     },
-//     {
-//         senderId: 'perborgen',
-//         text: 'Good to hear! I am great as well.'
-//     }
-// ]
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well.'
+    }
+]
 
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-            {this.props.messages.map((message, index) => {
+            {DUMMY_DATA.map((message, index) => {
+                return (
+                    <div key={index} className="message">
+                        <div className="message-username">{message.senderId}</div>
+                        <div className="message-text">{message.text}</div>
+                    </div>
+                )
+            })}
+            {/* {this.props.messages.map((message, index) => {
                 return (
                     <Message key={index} 
                         username={message.senderId} 
                         text={message.text}
                     />
                 )
-            })}
+            })} */}
             </div>
         )
     }
